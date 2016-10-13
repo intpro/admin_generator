93a24f71ed9f4388a5864d118495a395e6c66a43
InterProFront <fallzem@gmail.com>
2016-10-13 18:06:27 +0600

    fixes

--- src/Laravel/Blocks/images.php
+++ src/Laravel/Blocks/images.php
@@ -83,18 +83,10 @@ class images
                 <input type="hidden" class="icon_link" data-field-name="%name%" data-group="%group%" data-field-type="%type%" data-item-id="%id%" data-block="%block%" value="%icon%">
             </div>
             <div class="preview-block">
-            <label class="file-input">
-                <img src="/images/%preview%" class="preview" data-field-name="%name%" data-block="%block%"  data-group="%group%" data-item-id="%id%">
-<input type="file" accept="image/*" class="input_file group_field" data-field-name="%name%" data-field-type="%type%" data-block="%block%" data-group="%group%" data-item-id="%id%" data-entity="groupitem">                </label>
-            </div>
-
-                    <div class="preview-block">
-                        <label class="file-input">
-                            <img src="/images/%preview%" class="preview" data-field-name="%name%" data-block="%block%">
-                            <input type="file" accept="image/*" class="input_file group_field" data-field-name="%name%" data-field-type="%type%" data-block="%block%" data-group="%group%" data-item-id="%id%" data-entity="groupitem">
-                        </label>
+                    <label class="file-input">
+                        <img src="/images/%preview%" class="preview" data-field-name="%name%" data-block="%block%"  data-group="%group%" data-item-id="%id%">
+                        <input type="file" accept="image/*" class="input_file group_field" data-field-name="%name%" data-field-type="%type%" data-block="%block%" data-group="%group%" data-item-id="%id%" data-entity="groupitem">                </label>
                     </div>
-
                     <div class="action-block">
                         <label class="alt-title">Альтернативный текст (если изображение не удалось загрузить)</label>
                         <input type="text" placeholder="alt текст" class="alt-text" value="%alt%" data-item-id="%id%" data-field-name="%name%" data-block="%block%">
